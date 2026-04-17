b7aa02d6ecc69f01cf445db34328856ae20144bf
A.J. Psyk <ajpsyk@gmail.com>
2026-04-16 17:17:58 -0700

    meteoroid spawn script

--- actors/meteorite.gd
+++ actors/meteorite.gd
@@ -1,93 +1,95 @@
 extends Node2D
 
-const METEOROID_SPRITES := [
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_big1.png"),
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_big2.png"),
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_big3.png"),
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_big4.png"),
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_med1.png"),
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_med3.png"),
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_small1.png"),
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_small2.png"),
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_tiny1.png"),
-	preload("res://assets/kenney_space-shooter-remastered/PNG/Meteors/meteorBrown_tiny2.png")
-]
+var screen_size : Vector2
 
+@export var METEOROID_TEMPLATES: Array[MeteoroidResource] = []
+
+const COUNT: int = 50
+const SPEED_MIN = 20
+const SPEED_MAX = 80
 enum Polarity { BLUE, RED }
 
 const COLOR_BLUE = Color(0.2, 0.5, 1.0)
 const COLOR_RED = Color(1.0, 0.2, 0.2)
 
+
+var meteoroids: Array[Meteoroid] = []
+
 class Meteoroid:
-	var position := Vector2()
-	var speed := 1.0
+	var position : Vector2 = Vector2()
+	var speed : float = 1.0
 	var body := RID()
+	var hitbox := RID()
 	var texture: Texture2D
 	var polarity: Polarity
 	var display_color: Color
-	var active := false
+	var active : bool = false
 
 
 func _ready() -> void:
-	shape = PhysicsServer2D.circle_shape_create()
-	# Set the collision shape's radius for each bullet in pixels.
-	PhysicsServer2D.shape_set_data(shape, 8)
+	
+	screen_size = get_viewport_rect().size
+	
+	for _i in COUNT:
+		var meteoroid := Meteoroid.new()
+		
+		meteoroid.polarity = Polarity.values().pick_random()
+		if meteoroid.polarity == Polarity.RED:
+			meteoroid.display_color = COLOR_RED
+		else:
+			meteoroid.display_color = COLOR_BLUE
+		
+		var template = METEOROID_TEMPLATES.pick_random()
+		meteoroid.texture = template.texture
+		meteoroid.speed = randf_range(SPEED_MIN, SPEED_MAX)
+		
+		meteoroid.hitbox = PhysicsServer2D.circle_shape_create()
+		PhysicsServer2D.shape_set_data(meteoroid.hitbox, template.radius)
+		
+		meteoroid.body = PhysicsServer2D.body_create()
+		PhysicsServer2D.body_set_space(meteoroid.body, get_world_2d().get_space())
+		PhysicsServer2D.body_add_shape(meteoroid.body, meteoroid.hitbox)
+		PhysicsServer2D.body_set_collision_mask(meteoroid.body, 0)
 
-	for _i in BULLET_COUNT:
-		var b := Bullet.new()
-		# Give each bullet its own random speed.
-		bullet.speed = randf_range(SPEED_MIN, SPEED_MAX)
-		bullet.body = PhysicsServer2D.body_create()
-
-		PhysicsServer2D.body_set_space(bullet.body, get_world_2d().get_space())
-		PhysicsServer2D.body_add_shape(bullet.body, shape)
-		# Don't make bullets check collision with other bullets to improve performance.
-		PhysicsServer2D.body_set_collision_mask(bullet.body, 0)
-
-		# Place bullets randomly on the viewport and move bullets outside the
-		# play area so that they fade in nicely.
-		bullet.position = Vector2(
-				randf_range(0, get_viewport_rect().size.x) + get_viewport_rect().size.x,
-				randf_range(0, get_viewport_rect().size.y)
+		meteoroid.position = Vector2(
+				randf_range(0, get_viewport_rect().size.x),
+				randf_range(-1000, 0)
 			)
 		var transform2d := Transform2D()
-		transform2d.origin = bullet.position
-		PhysicsServer2D.body_set_state(bullet.body, PhysicsServer2D.BODY_STATE_TRANSFORM, transform2d)
+		transform2d.origin = meteoroid.position
+		PhysicsServer2D.body_set_state(meteoroid.body, PhysicsServer2D.BODY_STATE_TRANSFORM, transform2d)
 
-		bullets.push_back(bullet)
+		meteoroids.push_back(meteoroid)
 
 
 func _process(_delta: float) -> void:
-	# Order the CanvasItem to update every frame.
 	queue_redraw()
 
 
 func _physics_process(delta: float) -> void:
 	var transform2d := Transform2D()
-	var offset := get_viewport_rect().size.x + 16
-	for bullet: Bullet in bullets:
-		bullet.position.x -= bullet.speed * delta
+	
+	for meteoroid: Meteoroid in meteoroids:
+		meteoroid.position.y += meteoroid.speed * delta
 
-		if bullet.position.x < -16:
-			# Move the bullet back to the right when it left the screen.
-			bullet.position.x = offset
+		if meteoroid.position.y > screen_size.y + 100:
+			meteoroid.position = Vector2(
+				randf_range(0, screen_size.x),
+				-100
+			)
 
-		transform2d.origin = bullet.position
-		PhysicsServer2D.body_set_state(bullet.body, PhysicsServer2D.BODY_STATE_TRANSFORM, transform2d)
+		transform2d.origin = meteoroid.position
+		PhysicsServer2D.body_set_state(meteoroid.body, PhysicsServer2D.BODY_STATE_TRANSFORM, transform2d)
 
-
-# Instead of drawing each bullet individually in a script attached to each bullet,
-# we are drawing *all* the bullets at once here.
 func _draw() -> void:
-	var offset := -bullet_image.get_size() * 0.5
-	for bullet: Bullet in bullets:
-		draw_texture(bullet_image, bullet.position + offset)
+	for meteoroid: Meteoroid in meteoroids:
+		var tex_size = meteoroid.texture.get_size()
+		var rect = Rect2(meteoroid.position - tex_size * 0.5, tex_size)
+		draw_texture_rect(meteoroid.texture, rect, false, meteoroid.display_color)
 
-
-# Perform cleanup operations (required to exit without error messages in the console).
 func _exit_tree() -> void:
-	for bullet: Bullet in bullets:
-		PhysicsServer2D.free_rid(bullet.body)
-
-	PhysicsServer2D.free_rid(shape)
-	bullets.clear()
+	for meteoroid: Meteoroid in meteoroids:
+		PhysicsServer2D.free_rid(meteoroid.body)
+		PhysicsServer2D.free_rid(meteoroid.hitbox)
+	
+	meteoroids.clear()
