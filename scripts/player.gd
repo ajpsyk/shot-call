a73d4c387a7b3a2c54e202ebb5cbf2bb6c32d69b
coleyjpak <235602442+coleyjpak@users.noreply.github.com>
2026-04-29 16:06:15 -0700

    Update player.gd to use snake_case instead of camelCase

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,82 +5,83 @@ extends CharacterBody2D
 # ~ Game Logic ~
 
 ## Indicates which player number the player is (1 or 2) or that the player is playing singleplayer (0).
-@export var playerNumber: int = 0
+@export var player_number: int = 0
 
 ## The speed at which the player moves in pixels/second.
-@export var moveSpeed: float = 300
+@export var move_speed: float = 300
 
-@export var Laser: Node2D
+@export var laser: Node2D
 
-@export var fireDelay: float = 0.15
+@export var fire_delay: float = 0.15
 
 
 var polarity: Globals.Polarity = Globals.Polarity.RED
-var fireTimer: float = 0.0
+var fire_timer: float = 0.0
 
 # ~ Child Node References ~
-@onready var redShipSprite: Sprite2D = $RedShipSprite
-@onready var blueShipSprite: Sprite2D = $BlueShipSprite
+@onready var red_ship_sprite: Sprite2D = $RedShipSprite
+@onready var blue_ship_sprite: Sprite2D = $BlueShipSprite
 
-func _ready() -> void:
-	UpdateShipSprite()
 
 # ~~~~~ FUNCTIONALITY ~~~~~
 
+func _ready() -> void:
+	update_ship_sprite()
+
 func _physics_process(delta: float) -> void:
-	var xMovement: float 
-	var yMovement: float 
-	if playerNumber == 1:
-		xMovement = Input.get_axis("p1_left", "p1_right")
-		yMovement = Input.get_axis("p1_up", "p1_down")
-	elif playerNumber == 2:
-		xMovement = Input.get_axis("p2_left", "p2_right")
-		yMovement = Input.get_axis("p2_up", "p2_down")
+	var x_movement: float 
+	var y_movement: float 
+	if player_number == 1:
+		x_movement = Input.get_axis("p1_left", "p1_right")
+		y_movement = Input.get_axis("p1_up", "p1_down")
+	elif player_number == 2:
+		x_movement = Input.get_axis("p2_left", "p2_right")
+		y_movement = Input.get_axis("p2_up", "p2_down")
 
-	var direction: Vector2 = Vector2(xMovement, yMovement).normalized()
+	var direction: Vector2 = Vector2(x_movement, y_movement).normalized()
 
-	velocity = direction * moveSpeed
+	velocity = direction * move_speed
 	
-	fireTimer += delta
+	fire_timer += delta
 
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
 	# Handle action
-	if playerNumber == 1 and event.is_action_pressed("p1_action"):
+	if player_number == 1 and event.is_action_pressed("p1_action"):
 		get_viewport().set_input_as_handled()
-		Action()
-	if playerNumber == 2 and event.is_action_pressed("p2_action"):
+		action()
+	if player_number == 2 and event.is_action_pressed("p2_action"):
 		get_viewport().set_input_as_handled()
-		Action()
+		action()
 	
-	if (playerNumber == 1 and event.is_action_pressed("p1_fire")):
+	if (player_number == 1 and event.is_action_pressed("p1_fire")):
 		get_viewport().set_input_as_handled()
-		Fire()
-	if (playerNumber == 2 and event.is_action_pressed("p2_fire")):
+		fire()
+	if (player_number == 2 and event.is_action_pressed("p2_fire")):
 		get_viewport().set_input_as_handled()
-		Fire()
+		fire()
 
 
-func Fire():
-	if Laser and fireTimer >= fireDelay:
-		Laser.fire_laser(global_position, polarity)
-		fireTimer = 0.0
+func fire():
+	if laser and fire_timer >= fire_delay:
+		laser.fire_laser(global_position, polarity)
+		fire_timer = 0.0
 
-func Action():
+func action():
 	# Only does a simple polarity switch for now. This is how it can function in singleplayer.
 	if polarity == Globals.Polarity.RED:
 		polarity = Globals.Polarity.BLUE
 	elif polarity == Globals.Polarity.BLUE:
 		polarity = Globals.Polarity.RED
 
-	UpdateShipSprite()
+	update_ship_sprite()
 
 ## Called after Polarity updates, and in _ready() to set to initial polarity.
-func UpdateShipSprite():
+func update_ship_sprite():
 	if polarity == Globals.Polarity.RED:
-		blueShipSprite.set_visible(false)
-		redShipSprite.set_visible(true)
+		blue_ship_sprite.set_visible(false)
+		red_ship_sprite.set_visible(true)
 	elif polarity == Globals.Polarity.BLUE:
-		redShipSprite.set_visible(false)
-		blueShipSprite.set_visible(true)
+		red_ship_sprite.set_visible(false)
+		blue_ship_sprite.set_visible(true)
