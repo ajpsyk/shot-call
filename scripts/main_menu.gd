de653eca5aa633556b03787fb3efd65a37e43a07
A.J. Psyk <ajpsyk@gmail.com>
2026-05-15 22:40:51 -0700

    first 2 minutes of stage 1

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,12 +3,12 @@ extends Node2D
 
 func _on_single_player_pressed() -> void:
 	Globals.multiplayer_mode = false
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
 
 func _on_coop_pressed() -> void:
 	Globals.multiplayer_mode = true
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
 
 func _on_quit_pressed() -> void:
