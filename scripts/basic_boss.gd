0d6841b653c83528e925ff793ca36bc46cde163d
Cole Pak <235602442+coleyjpak@users.noreply.github.com>
2026-05-28 12:07:25 -0700

    Add animations and sound effects to Basic Boss

--- scripts/basic_boss.gd
+++ scripts/basic_boss.gd
@@ -48,6 +48,16 @@ var curr_state: BASIC_BOSS_STATE = BASIC_BOSS_STATE.INTRO
 
 @onready var white_flash: Sprite2D = $Sprite2D/WhiteFlash
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@onready var fire_sound: AudioStreamPlayer2D = $FireSound
+@onready var polarity_swap_sound: AudioStreamPlayer2D = $PolaritySwapSound
+@onready var lock_on_sound: AudioStreamPlayer2D = $LockOnSound
+@onready var charge_prep_sound: AudioStreamPlayer2D = $ChargePrepSound
+@onready var charge_dash_sound: AudioStreamPlayer2D = $ChargeDashSound
+@onready var hurt_sound: AudioStreamPlayer2D = $HurtSound
+@onready var absorb_sound: AudioStreamPlayer2D = $AbsorbSound
+
 @onready var health_rich_text_label: RichTextLabel = $HealthLeft #DEBUG
 
 
@@ -92,7 +102,7 @@ func _physics_process(delta: float) -> void:
 
 		# Lock onto a player and charge at them, then reappear at the top of the screen
 		BASIC_BOSS_STATE.CHARGE:
-			if velocity.is_zero_approx() and targeted_player:
+			if !lock_on_delay_timer.is_stopped() and targeted_player:
 				look_at(targeted_player.global_position)
 		
 	move_and_slide()
@@ -100,9 +110,9 @@ func _physics_process(delta: float) -> void:
 	if not enraged and health <= max_health * enraged_health_percent:
 		enraged = true
 		curr_speed = base_speed * speed_mult_enraged
-		basic_state_timer.stop()
-		lock_on_delay_timer.stop()
-		lock_on_state_timer.stop()
+		change_state(BASIC_BOSS_STATE.NONE)
+		animation_player.play("enraged_transition")
+		await animation_player.animation_finished
 		change_state(BASIC_BOSS_STATE.CHARGE)
 
 
@@ -119,11 +129,14 @@ func change_state(state: BASIC_BOSS_STATE):
 			polarity_swap_timer.stop()
 
 		BASIC_BOSS_STATE.LOCK_ON:
-			global_rotation = 0
 			fire_timer.stop()
 
 		BASIC_BOSS_STATE.CHARGE:
-			velocity = Vector2(0,0)
+			pass
+
+		BASIC_BOSS_STATE.NONE:
+			pass
+
 
 	curr_state = state
 
@@ -139,19 +152,31 @@ func change_state(state: BASIC_BOSS_STATE):
 
 			polarity_swap_timer.start()
 			basic_state_timer.start()
-			velocity.x = curr_speed
+			velocity = Vector2(curr_speed, 0)
 
 		BASIC_BOSS_STATE.LOCK_ON:
 			print("[Basic Boss] Players: ", Globals.players)
 			targeted_player = Globals.players.pick_random()
-			velocity = Vector2(0,0)
+			velocity = Vector2.ZERO
 			lock_on_delay_timer.start()
 			lock_on_state_timer.start()
+			lock_on_sound.play()
 
 		BASIC_BOSS_STATE.CHARGE:
 			targeted_player = Globals.players.pick_random()
-			velocity = Vector2(0,0)
+			velocity = Vector2(0,-20)
 			lock_on_delay_timer.start()
+			charge_prep_sound.play()
+
+		BASIC_BOSS_STATE.NONE:
+			velocity = Vector2.ZERO
+			fire_timer.stop()
+			polarity_swap_timer.stop()
+			basic_state_timer.stop()
+			lock_on_state_timer.stop()
+			lock_on_delay_timer.stop()
+			post_charge_daze_timer.stop()
+			animation_player.play("RESET")
 		
 
 func change_polarity() -> void:
@@ -164,10 +189,28 @@ func change_polarity() -> void:
 		polarity = Globals.Polarity.BLUE
 	update_polarity_color()
 
+	polarity_swap_sound.play()
+
 func take_damage_effect() -> void:
 	var tween = create_tween()
 	tween.tween_property(white_flash, "modulate", Color(1,1,1,0.5), 0.05)
 	tween.tween_property(white_flash, "modulate", Color(1,1,1,0), 0.05)
+	hurt_sound.play()
+
+func absorb_damage_effect() -> void:
+	super()
+	absorb_sound.play()
+
+
+func die(damage_source = null) -> void:
+	set_hurtbox_active(false)
+	change_state(BASIC_BOSS_STATE.NONE)
+	if damage_source and "shooter_id" in damage_source:
+		award_points(damage_source.shooter_id)
+	animation_player.play("death")
+	await animation_player.animation_finished
+	queue_free()
+
 
 func _on_laser_fire_timer_timeout() -> void:
 	var new_laser: Area2D = laser_scene.instantiate()
@@ -175,6 +218,8 @@ func _on_laser_fire_timer_timeout() -> void:
 	new_laser.global_position = global_position
 	new_laser.global_rotation = global_rotation
 	get_tree().root.add_child(new_laser)
+	animation_player.play("fire_recoil")
+	fire_sound.play()
 
 func _on_polarity_swap_timer_timeout() -> void:
 	change_polarity()
@@ -196,14 +241,16 @@ func _on_lock_on_delay_timer_timeout() -> void:
 	if curr_state == BASIC_BOSS_STATE.LOCK_ON:
 		fire_timer.start(fire_rate_lock_on)
 	elif curr_state == BASIC_BOSS_STATE.CHARGE:
+		charge_dash_sound.play()
 		velocity = Vector2.from_angle(global_rotation).normalized() * curr_speed * speed_mult_charge
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	global_rotation = 90
-	position.y = -50
+	position.y = -100
 	position.x = clampf(position.x, left_edge, right_edge)
-	velocity = Vector2.DOWN * curr_speed * 0.2
-	post_charge_daze_timer.start()
+	if curr_state == BASIC_BOSS_STATE.CHARGE:
+		velocity = Vector2.DOWN * curr_speed * 0.2
+		post_charge_daze_timer.start()
 
 func _on_post_charge_daze_timer_timeout() -> void:
 	change_state(BASIC_BOSS_STATE.BASIC)
